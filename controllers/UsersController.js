b803e1d0031cfe23818ebb14c37263f384e379b7
Bamiwo Adebayo <bammietop@gmail.com>
2024-06-15 23:40:18 +0100

    file manager

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.db.collection('users').findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashPassword = sha1(password);
+
+    const result = await dbClient.db.collection('users').insertOne({ email, password: hashPassword });
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+export default UsersController;
