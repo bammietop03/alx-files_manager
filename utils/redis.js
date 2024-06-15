d026b74227ba448f7a3a9fcd0d5bfb2b6c36746e
Bamiwo Adebayo <bammietop@gmail.com>
2024-06-15 20:29:36 +0100

    File Manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,59 +1,57 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
+  constructor() {
+    this.client = createClient();
 
-        this.client.on('connect', () => {
-            // console.log("Redis client connected to the server");
-        })
+    this.client.on('connect', () => {
+      // console.log("Redis client connected to the server");
+    });
 
-        this.client.on('error', (error) => {
-            console.log(error)
-        })
+    this.client.on('error', (error) => {
+      console.log(error);
+    });
+  }
 
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
-    
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, reply) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(reply);
-                }
-            });
-        });
-    }
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.set(key, value, 'EX', duration, (err, reply) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(reply)
-                }
-            });
-        });
-    }
-
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err, reply) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(reply)
-                }
-            });
-        });
-    }
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
